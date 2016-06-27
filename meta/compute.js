a9bf595d2ddef86f7d5979e3148521d5a7fcaf98
Jessica Kerr <jessitron@stripe.com>
2016-06-26 19:16:06 -0500

    move function inside

--- meta/compute.js
+++ meta/compute.js
@@ -1,10 +1,12 @@
-function camelize(str) {  // http://stackoverflow.com/questions/2970525/converting-any-string-into-camel-case
-  return str.replace(/(?:^\w|[A-Z]|\b\w)/g, function(letter, index) {
-    return index == 0 ? letter.toLowerCase() : letter.toUpperCase();
-  }).replace(/\s+/g, '');
-}
+
 
 function computed_parameters(existing_parameters) {
+
+  function camelize(str) {  // http://stackoverflow.com/questions/2970525/converting-any-string-into-camel-case
+    return str.replace(/(?:^\w|[A-Z]|\b\w)/g, function(letter, index) {
+    return index == 0 ? letter.toLowerCase() : letter.toUpperCase();
+    }).replace(/\s+/g, '');
+  }
 
   // also need to mush out dashes, make a nice camel case
   var elmModuleName = camelize(existing_parameters["name"])

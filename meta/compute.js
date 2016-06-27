468c43f96fa33e7a6c80b0cbc562af858fe19256
Jessica Kerr <jessitron@stripe.com>
2016-06-26 19:08:37 -0500

    Oh right, forgot to save this one

--- meta/compute.js
+++ meta/compute.js
@@ -1,9 +1,13 @@
-function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
+function camelize(str) {  // http://stackoverflow.com/questions/2970525/converting-any-string-into-camel-case
+  return str.replace(/(?:^\w|[A-Z]|\b\w)/g, function(letter, index) {
+    return index == 0 ? letter.toLowerCase() : letter.toUpperCase();
+  }).replace(/\s+/g, '');
 }
 
 function computed_parameters(existing_parameters) {
 
-  // also need to mush out dashes
-  var elmModuleName = capitalizeFirstLetter(existing_parameters["name"])
+  // also need to mush out dashes, make a nice camel case
+  var elmModuleName = camelize(existing_parameters["name"])
+
+  return { module_name : elmModuleName }
 }

b3b61e40627247c6cb2ac8fc2f50ca4509623ba5
Jessica Kerr <jessitron@stripe.com>
2016-06-26 19:18:28 -0500

    try something stupid

--- meta/compute.js
+++ meta/compute.js
@@ -1,6 +1,6 @@
 
 
-function computed_parameters(existing_parameters) {
+function computed_parameters(m) {
 
   function camelize(str) {  // http://stackoverflow.com/questions/2970525/converting-any-string-into-camel-case
     return str.replace(/(?:^\w|[A-Z]|\b\w)/g, function(letter, index) {
@@ -9,7 +9,7 @@ function computed_parameters(existing_parameters) {
   }
 
   // also need to mush out dashes, make a nice camel case
-  var elmModuleName = camelize(existing_parameters["name"])
+  var elmModuleName = camelize(m["name"])
 
   return { "module_name" : elmModuleName }
 }
